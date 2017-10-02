d520e7743b71a52d64ebb1ec03d4ec8a86651f5b
John Anderson <madroxprime@gmail.com>
2017-10-02 09:33:30 -0500

    Standards
    Replaced usage of var with Const and Let

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var sqlite = require("sqlite3");
+const sqlite = require("sqlite3");
 
 function findComputerName(options,cb){
-    if(options.database == null || options.user == null){
+    if(options.database !== null && options.user !== null){
         cb({Error:"Options must contain database and user property."});
     }
-    var database = options.database;
-    var user = options.user;
-    var db = new sqlite.Database(path);
+    let database = options.database;
+    let user = options.user;
+    let db = new sqlite.Database(database);
 
-    db.all("SELECT DISTINCT computerName FROM test WHERE userName = "+user+" ORDER BY date DESC LIMIT 5",
+    db.all("SELECT DISTINCT computerName FROM test WHERE userName = "+user+" ORDER BY loginDate DESC LIMIT 5",
         function(err,rows){
             if(err) cb(err);
             if(rows){
