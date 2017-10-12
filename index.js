50eb3a6dec68a046124fb09b22600f8c351af45f
John Anderson <madroxprime@gmail.com>
2017-10-12 11:40:45 -0500

    minor changes to conform to style guide

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const sqlite = require("sqlite3");
+const sqlite = require('sqlite3');
 
 function findComputerName(options,cb){
     if(options.database !== null && options.user !== null){
-        cb({Error:"Options must contain database and user property."});
+        cb({Error:'Options must contain database and user property.'});
     }
     let database = options.database;
     let user = options.user;
     let db = new sqlite.Database(database);
 
-    db.all("SELECT DISTINCT computerName FROM test WHERE userName = "+user+" ORDER BY loginDate DESC LIMIT 5",
+    db.all('SELECT DISTINCT computerName FROM test WHERE userName = '+user+' ORDER BY loginDate DESC LIMIT 5',
         function(err,rows){
             if(err) cb(err);
             if(rows){
                 cb(null,rows);
             }
-    })
+        });
 }
 
 module.exports = findComputerName;
